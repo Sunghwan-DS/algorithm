2aeac8b4d2d11f79886f9148aa380ed7d81a3e67
Sunghwan-DS <jeonsung02@gmail.com>
2024-05-05 13:00:51 +0900

    java.util.Scanner

---
+++ src/main/java/jsh/algorithm/string/MyScanner.java
@@ -0,0 +1,15 @@
+package jsh.algorithm.string;
+
+import java.util.Scanner;
+
+public class MyScanner {
+
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a + b = " + (a + b));
+    }
+}
